23919f48a414ef1cad6fb14e307f513b2bd7f4ae
Gagan Mann <GaGaNmAnn@Gagans-MBP.attlocal.net>
2021-07-01 13:37:24 -0700

    Initial Commit

---
+++ HelloWorld/src/Remove_duplicates.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Remove_duplicates {
+
+	public static <E> void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<E> list = new ArrayList<E>();
+		
+		
+		
+		
+	}
+
+}
---
+++ Maven_ExtentReports/src/test/java/Extent_Reports/Theory.java
@@ -0,0 +1,14 @@
+package Extent_Reports;
+
+/*Extent Reports-- In our Framework development, we mostly use extent reports to receive 
+  the execution results of all our test cases,	
+ *Understanding basic configuration of extent reports with stand-alone test.
+  
+ */
+
+
+
+
+public class Theory {
+
+}
